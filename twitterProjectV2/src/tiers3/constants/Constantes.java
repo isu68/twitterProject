61e52f9f85f66c602ac75993f44b38d3501d0578
Flo <Flo@130.190.44.17>
2014-05-22 13:23:30 +0200

    commit

---
+++ twitterProjectV2/src/tiers3/constants/Constantes.java
@@ -0,0 +1,18 @@
+package tiers3.constants;
+
+public class Constantes {
+
+	public final static int PORT_SERVEUR = 2000;
+
+	public final static String NAMESPACE_SERVEUR = "ServicePersistance";
+	
+	public final static String JAXB_PROPRIETE_ENCODING = "jaxb.encoding";
+
+	public final static String JAXB_PROPRIETE_ENCODING_VALEUR = "UTF-8";
+
+	public final static String JAXB_PROPRIETE_FORMATTED = "jaxb.formatted.output";
+	
+	public final static String UTILISATEUR_PREFIXE_SERIALISATION = "utilisateur";
+	
+	public final static String SUFFIXE_XML = ".xml";
+}
